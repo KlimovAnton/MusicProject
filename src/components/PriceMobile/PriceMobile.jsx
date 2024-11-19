a65f7974c495023af7d0ab5cd258b6846cf62240
Anton <antonklim6@gmail.com>
2024-11-19 22:31:20 +0100

    price mob

--- src/components/PriceMobile/PriceMobile.jsx
+++ src/components/PriceMobile/PriceMobile.jsx
@@ -5,10 +5,15 @@ import img2 from '../../assets/beach-photo-own.jpg';
 
 export default function PriceMobile () {
     return (
-        <Link className={css.link} to="/price">
+        <section className='section'>
+            <div className={css.container}>
+                <Link className={css.link} to="/price">
                     <div className={css.overlay}></div>
                     <img className={css.img} src={img2} alt="photo-own-music"/>
                     <p className={css.text}>Prices</p>
-        </Link>
+                </Link>
+            </div>
+        </section>
+        
     )
 }
