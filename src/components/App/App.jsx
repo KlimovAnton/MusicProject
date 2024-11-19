baafb336ae12de6c5008b9cf378bf1083d674191
Anton <antonklim6@gmail.com>
2024-11-19 13:57:25 +0100

    gallery delete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,6 @@ const HomePage = lazy (() => import('../../pages/HomePage/HomePage'));
 const AboutPage = lazy (() => import('../../pages/AboutPage/AboutPage'));
 const PricePage = lazy (() => import('../../pages/PricePage/PricePage'));
 const VideoPage = lazy (() => import('../../pages/VideoPage/VideoPage'));
-const GalleryPage = lazy (() => import('../../pages/GalleryPage/GalleryPage'));
 const ContactPage = lazy (() => import('../../pages/ContactPage/ContactPage'));
 
 function App() {
@@ -20,7 +19,6 @@ function App() {
             <Route path="/about" element={<AboutPage />}/>
             <Route path="/price" element={<PricePage />}/>
             <Route path="/video" element={<VideoPage />}/>
-            <Route path="/gallery" element={<GalleryPage />}/>
             <Route path="/contacts" element={<ContactPage />}/>
           </Routes>
         </Suspense>
