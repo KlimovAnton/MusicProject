f469ceb5161dac18e7f0d4d3f3e8cec0d4f50585
Anton <antonklim6@gmail.com>
2024-11-29 19:53:43 +0100

    form style

--- src/components/Contact/ContactForm/ContactForm.jsx
+++ src/components/Contact/ContactForm/ContactForm.jsx
@@ -1,62 +1,74 @@
 import css from './ContactForm.module.css';
+import clsx from 'clsx';
 
-import CustomInput from '../../Input/CustomInput';
-import ButtonSend from '../../ButtonSend/ButtonSend'
-import { useForm } from 'react-hook-form';
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+import ButtonSend from '../../ButtonSend/ButtonSend';
+import { FormValidation } from '../../../Validation/ValidationForm';
+
+
 export default function ContactForm () {
 
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     formState: { errors, isDirty, isValid },
-    //   } = useForm({
-    //     mode: 'onChange',
-    //     resolver: yupResolver(signInFormValidation),
-    //   });
+    const initialValues = {
+      email: "",
+      name: "",
+      phone: "",
+    };
 
-    const handleSubmit = (evt) => {
-        evt.preventDefault();
-
-        const form = evt.target;
-        const { email, name, message } = form.elements;
-
-        console.log(email.value, name.value, message.value)
-        form.reset();
-    }
-
-    
+    const handleSubmit = async (value, actions) => {
+        console.log(value)
+        actions.resetForm();
+    };
 
     return (
         <div className={css.containerForm}>
             <h2 className={css.title}>Fill out this form:</h2>
-            <form className={css.form}
-            onSubmit={handleSubmit}>
-                <CustomInput 
-                labelName="Email"
-                inputType="email"
-                name="email"
-                labelClass={css.label}
-                inputClass={css.input}
-                placeholder="Enter your email"
-                />
-                <CustomInput 
-                labelName="Name"
-                inputType="text"
-                name="name"
-                labelClass={css.label}
-                inputClass={css.input}
-                placeholder="Enter your name"
-                />
-                <CustomInput
-                labelName="Message"
-                inputType="text"
-                name="message"
-                labelClass={css.label}
-                inputClass={css.input}
-                placeholder="Enter your message"
-                />
-                <ButtonSend type="submit">Send</ButtonSend>
-            </form>
+            <Formik
+                initialValues={initialValues}
+                onSubmit={handleSubmit}
+                validationSchema={FormValidation}
+                validateOnMount
+                >
+            {formik => {
+                return (
+                    <Form className={css.form} autoComplete="off">
+                        <label className={css.label}>Your Email</label>
+                        <Field
+                            className={clsx(css.input, css.inputError)}
+                            label="Email"
+                            type="email"
+                            name="email"
+                            autoComplete="off"
+                            required
+                            placeholder="mail@mail.com"
+                        />
+                        <ErrorMessage className={css.error} name="email" component="span" />
+                        
+                        <label className={css.label}>Your Name</label>
+                        <Field
+                            className={clsx(css.input, css.inputError)}
+                            label="Name"
+                            name="name"
+                            autoComplete="current-name"
+                            required
+                            placeholder="Andrew Neiman"
+                        />
+                        <ErrorMessage className={css.error} name="name" component="span" />
+                        
+                        <label className={css.label}>Your Phone</label>
+                        <Field
+                            className={clsx(css.input, css.inputError)}
+                            label="Phone"
+                            name="phone"
+                            required
+                            placeholder="43 677 00000000"/>
+                        <ErrorMessage className={css.error} name="phone" component="span" />
+
+                        <ButtonSend type="submit" disabled={!(formik.dirty && formik.isValid)}>Send</ButtonSend>
+                    </Form>
+                )
+            }}
+          </Formik>
         </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './components/App/App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
