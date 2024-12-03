fbc9678ee546c1d5aa0f374ad0bc68e75ca0adea
Anton <antonklim6@gmail.com>
2024-12-03 21:29:34 +0100

    yup

--- src/Validation/ValidationForm.js
+++ src/Validation/ValidationForm.js
@@ -1,17 +1,17 @@
-import * as yup from 'Yup';
+import * as Yup from 'Yup';
 
 const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
-export const FormValidation = yup.object().shape({
-    email: yup
+export const FormValidation = Yup.object().shape({
+    email: Yup
       .string()
       .email('Email is invalid, use format @mail.com')
       .required("Email is required"),
-    name: yup
+    name: Yup
       .string()
       .max(30, "Name must be max 30 characters")
       .required("Name is required"),
-    phone: yup
+    phone: Yup
       .string()
       .matches(phoneRegExp, "Phone number is not valid, example: 4367711111111")
       .required("Phone is required")
